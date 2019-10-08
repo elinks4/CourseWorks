7203280e9bbd5bf1e2f6cd9487c9b6a6336be109
elinks4 <46779843+elinks4@users.noreply.github.com>
2019-10-09 00:30:06 +0300

    Teht 1

---
+++ CodesonVisualStudioCode/BikeRentalStationList.cs
@@ -0,0 +1,19 @@
+namespace CodesonVisualStudioCode
+{
+    public class BikeRentalStationList
+    {
+
+        public stations[] stations {get; set;}
+
+
+
+    }
+
+
+
+    public class stations{
+
+        public int bikesAvailable{get; set;}
+        public string name{get; set;}
+    }
+}
---
+++ CodesonVisualStudioCode/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodesonVisualStudioCode;
+
+namespace Codes_on_Visual_Studio_Code
+{
+
+
+
+
+    class Program
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args[0]);
+
+            string teststring;
+            string townstring;
+
+
+            Console.WriteLine("do you want offline or online = off/on");
+            teststring = Console.ReadLine();
+
+            if (teststring == "off")
+            {
+
+
+                Console.WriteLine("What street are you looking for: ");
+                townstring = Console.ReadLine();
+
+                OfflineCityBikeDataFetcher offline = new OfflineCityBikeDataFetcher();
+
+                try
+                {
+                    Console.WriteLine(await offline.GetBikeCountInStation(townstring));
+
+                }
+
+                catch (NotFoundException e)
+                {
+
+                    Console.WriteLine("Not Found " + e.Message);
+
+
+
+                }
+
+            }
+
+            if (teststring == "on")
+            {
+                Console.WriteLine("What street are you looking for: ");
+                townstring = Console.ReadLine();
+                RealTimeCityBikeDataFetcher realTime = new RealTimeCityBikeDataFetcher();
+                try
+                {
+                    Console.WriteLine(await realTime.GetBikeCountInStation(townstring));
+                }
+                catch (NotFoundException e)
+                {
+
+                    Console.WriteLine("Not Found " + e.Message);
+
+
+
+                }
+
+
+            }
+            if (teststring != "off" && teststring != "on")
+            {
+
+                Console.WriteLine("wrote something else");
+
+
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
+
